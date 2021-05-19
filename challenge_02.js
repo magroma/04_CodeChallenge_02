dbf2ffb98d94c46c8e65ccacacea190a31b67495
Marco <marco_2@gmx.de>
2021-05-19 20:21:03 +0200

    switch / case um m/w Abfrage erweitert

--- challenge_02.js
+++ challenge_02.js
@@ -18,36 +18,59 @@ Alter 13-17 // Cola
 
 /********** Switch|Case ***********/
 
+let gender = prompt("Ist die Person männlich (m) oder weiblich (w)?");
 let name = prompt("Wie heißt die Person, \ndie etwas trinken möchte?");
-let age = parseInt(prompt("Wie alt ist Person die etwas trinken möchte?"));
+let age = parseInt(prompt("Wie alt ist " + name + "?"));
+    
 
 switch (true) {
     case (age >= 0 && age <= 5):
     //case (age <= 5):
-        console.log(name + " ist " + age + " und trinkt Milch und Wasser");
+        console.log("Für " + name + " haben wir Milch und Wasser im Angebot");
         break;
-    //case (age >= 6 && age <= 12):
-    case (age <= 12):
+    case (age >= 6 && age <= 12 && gender == "m"):
+    //case (age <= 12 && gender == "m"):
+        console.log(name + " ist " + age + " und trinkt Saft, Wasser und Limo");
+        break;
+    case (age >= 6 && age <= 12 && gender == "w"):
+    //case (age <= 12 && gender == "m"):
         console.log(name + " ist " + age + " und trinkt Saft, Wasser und Limo");
         break;
     //case (age >= 13 && age <= 17):
-    case (age <= 17):
+    case (age >= 13 && age <= 17 && gender == "m"):
         console.log(name + " ist " + age + " und trinkt Cola, Wasser, Saft, Limo und Kaffee");
         break;
-    case (age >= 18 && age <=25):    
+    case (age >= 13 && age <= 17 && gender == "w"):
+        console.log(name + " ist " + age + " und trinkt Cola, Wasser, Saft, Limo und Kaffee");
+        break;
+    case (age >= 18 && age <=25 && gender == "m"):    
         console.log(name + " ist " + age + " und trinkt Alkopops, Bier und gesunde Getränke");
         break;
-    case (age >= 26 && age <= 90):    
+    case (age >= 18 && age <=25 && gender == "w"):    
+        console.log(name + " ist " + age + " und trinkt Alkopops, Bier und gesunde Getränke");
+        break;
+    case (age >= 26 && age <= 90 && gender == "m"):
         console.log(name + " ist " + age + " und trinkt Bier, Wein und was er will");
         break;
-    case (age > 90):    
+    case (age >= 26 && age <= 90 && gender == "w"):
+        console.log(name + " ist " + age + " und trinkt Bier, Wein und was sie will");
+        break;
+    case (age > 90 && gender == "m"):
+        /*if (gender == "männlich") 
+        {
+            console.log("Opa " + name + " ist " + age + " und kann trinken was er will, \nwenn er es verträgt");
+        } else 
+        {
+            console.log("Oma " + name + " ist " + age + " und kann trinken was sie will, \nwenn sie es verträgt");
+        }*/
         console.log("Opa " + name + " ist " + age + " und kann trinken was er will, \nwenn er es verträgt");
         break;
-
-
+    case (age > 90 && gender == "w"):
+        console.log("Oma " + name + " ist " + age + " und kann trinken was sie will, \nwenn sie es verträgt");
+        break;
 
     default:
-        console.log("ohne Alter gibt's auch nix zu trinken!")
+        console.log("Wenn nicht alles ausgefüllt ist gibt's auch nix zu trinken!")
         break;
 }
 
